6aca30fa916b3694b8b9f18861c965cfbac01b38
Michajster <95509732+Michajster@users.noreply.github.com>
2026-02-25 11:47:06 +0000

    gg

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,14 +1,29 @@
 
 // src/App.tsx
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useSearchParams } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LineDetailsPage from "./pages/LineDetailsPage";
 import { useLayoutLive } from "./hooks/useLayoutLive";
 import QrMagazynPage from "./pages/QrMagazynPage";
+import QrTimesPage from "./pages/QrTimesPage";
+import { useEffect } from "react";
+import { clearAllData } from "./services/clearAllData";
 
 
 export default function App() {
+  const [searchParams] = useSearchParams();
+  
+  useEffect(() => {
+    // Czyszczenie danych dla celów testowych: otwórz http://localhost:5175/#/?clear=true
+    if (searchParams.get('clear') === 'true') {
+      console.log('[TEST] Czyszczenie danych...');
+      clearAllData();
+      // Usuń parametr z URL
+      window.location.hash = '/';
+    }
+  }, [searchParams]);
+
   useLayoutLive(); // live update mapy (zostaje)
 
   return (
@@ -17,6 +32,7 @@ export default function App() {
       <Routes>
         
         <Route path="/qr/magazyn" element={<QrMagazynPage />} />
+  <Route path="/qr/times" element={<QrTimesPage />} />
    
         <Route path="/" element={<HomePage />} />
         <Route path="/line/:lineId" element={<LineDetailsPage />} />
